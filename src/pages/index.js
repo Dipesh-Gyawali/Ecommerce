c84ae1145c3737339047bb1503b401f1d2e9deed
Dipesh-Gyawali <dipeshgyawali365@gmail.com>
2023-07-06 18:24:20 +0545

    login and registration

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,6 @@ export { ProductsList } from "./Products/ProductsList";
 
 export { ProductDetail } from "./ProductDetail";
 export { PageNotFound } from "./PageNotFound";
+
+export { Login } from "./Login";
+export { Register } from "./Register";
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { HomePage, ProductsList } from "../pages";
 import { ProductDetail} from "../pages";
 import { PageNotFound} from "../pages";
+import { Login,Register } from "../pages";
 
 export const AllRoutes = () => {
   return (
@@ -9,9 +10,12 @@ export const AllRoutes = () => {
     <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductsList />} />
+
         <Route path="/products/:id" element={<ProductDetail />} />
         <Route path="*" element={<PageNotFound />} />
 
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
 
     </Routes>
     </>
