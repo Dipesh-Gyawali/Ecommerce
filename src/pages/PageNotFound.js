22e0562f13c2ff33d89301dde175cc7d9e995ff8
Dipesh-Gyawali <dipeshgyawali365@gmail.com>
2023-08-24 07:21:06 +0545

    toggle dropdown

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import PageNotFoundImage from "../assets/images/pagenotfound.png";
 
 export const PageNotFound = () => {
   return (
@@ -7,11 +8,11 @@ export const PageNotFound = () => {
             <div className="flex flex-col items-center my-4">
                 <p className="text-7xl text-gray-700 font-bold my-10 dark:text-white text-center">404, Oops!</p>
                 <div className="max-w-xs">
-                    <img className="a" src="aaaaaaa" alt="aaa" />
+                    <img className="rounded" src={PageNotFoundImage} alt="Page Not Found" />
                 </div>
             </div>
             <div className="flex justify-center my-4">
-                <Link href="">
+                <Link to="/">
                     <button type="button" className="w-64 text-2xl text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2">Back To Home</button>
                 </Link>
             </div>
